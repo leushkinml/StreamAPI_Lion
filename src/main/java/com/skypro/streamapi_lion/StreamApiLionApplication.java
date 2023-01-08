ba78c13816ec0b3d986558f5d4ec6737237cba1a
leush <leushkinml@mail.ru>
2023-01-09 00:43:50 +0300

    Initial commit

---
+++ src/main/java/com/skypro/streamapi_lion/StreamApiLionApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.streamapi_lion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApiLionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApiLionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/streamapi_lion/StreamApiLionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.streamapi_lion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApiLionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
